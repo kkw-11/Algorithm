c19e57b60d708a2c48601fd91143172003d75971
asus <kiwon1011@naver.com>
2021-01-06 12:41:47 +0900

    1/6 stack algorithm

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,326 +1,70 @@
-//56. 재귀함수 분석
+////53. K진수 출력
+////강의풀이
+//#include<stdio.h>
+//#include<stack>
+//using namespace std;
+//
+//int main() {
+//	//freopen("input.txt", "rt", stdin);
+//	int N, numsys, mod, quotient = 1;
+//	char str[20] = "0123456789ABCDEF";
+//	stack<int> s;
+//
+//	scanf("%d%d", &N, &numsys);
+//
+//
+//	quotient = N;
+//	while (quotient) {
+//		mod = quotient % numsys;
+//		s.push(mod);
+//		quotient /= numsys;
+//	}
+//
+//	while (!s.empty()) {
+//		printf("%c", str[s.top()]);
+//		s.pop();
+//	}
+//
+//
+//	return 0;
+//}
 
-#include <stdio.h>
+//53. K진수 출력
+#include<stdio.h>
+int stack[20];
+int top;
 
-void RecursionNumber(int n) {
-	if (n == 0)
-		return;
-
-	RecursionNumber(n - 1);
-
-	printf("%d ", n);
+void InitStack() {
+	top = -1;
 }
-
+void Push(int data) {
+	if (top == 19) return;
+	stack[++top] = data;
+}
+int Pop() {
+	if (top == -1) return -1;
+	return stack[top--];
+}
 
 int main() {
-//	freopen("input.txt", "rt", stdin);
-	int number;
-	scanf("%d", &number);
-	RecursionNumber(number);
+	//freopen("input.txt", "rt", stdin);
+	int N, numsys, mod, quotient = 1;
+
+	scanf("%d%d", &N, &numsys);
+	InitStack();
+	quotient = N;
+	while (quotient) {
+		mod = quotient % numsys;
+		Push(mod);
+		quotient /= numsys;
+	}
+
+	while (top != -1) {
+		if (numsys == 16)
+			printf("%X", Pop());
+		else
+			printf("%d", Pop());
+	}
+
+	return 0;
 }
-
-
-////7.영어단어복구-강의풀이
-//#include <stdio.h>
-//int main() {
-//	//freopen("input.txt", "rt", stdin);
-//	char str[101], restr[101];
-//
-//	gets_s(str);
-//	int j = 0;
-//	for (int i = 0; str[i]; ++i) {
-//		if (str[i] != ' ') {
-//			if (str[i] >= 65 && str[i] <= 90)//A: 65, Z: 90
-//				restr[j++] = str[i] + 32; //후위 증감 대입하고 j는 1 증가
-//			else
-//				restr[j++] = str[i]; 
-//		}
-//	}
-//	restr[j] = '\0'; //문자열 마지막에 널 문자 입력으로 %s로 출력하기 위해
-//	printf("%s",restr);
-//
-//	return 0;
-//}
-
-////7.영어단어복구
-//#include <stdio.h>
-//int main() {
-//	//freopen("input.txt", "rt", stdin);
-//	char str[101];
-//	gets_s(str);
-//
-//	for (int i = 0; str[i]; ++i) {
-//		if (str[i] >= 65 && str[i] <= 122) {
-//			{
-//				if (str[i] >= 65 && str[i] <= 90) {
-//					str[i] = str[i] + 32;
-//				}
-//				printf("%c", str[i]);
-//			}
-//		}
-//	}	
-//
-//	return 0;
-//}
-
-////6.숫자만 추출
-//#include<stdio.h>
-//
-//int main() {
-//	//freopen("input.txt", "rt", stdin);
-//	char ch[10];
-//	int num = 0;
-//	int ten = 0;
-//	int cnt = 0;
-//	scanf("%s", ch);
-//	for (int i = 0; ch[i]; ++i) {
-//		if (ch[i] >= 48 && ch[i] <= 57) {
-//			num = ((num * 10) + (ch[i] - 48));
-//			++ten;
-//		}
-//	}
-//
-//	int j = num;
-//	for (int i = 1; i < j; ++i) {
-//		if (num % i == 0) {
-//			j = num / i;
-//			cnt += 2;;
-//		}
-//	}
-//	printf("%d\n", num);
-//	printf("%d", cnt);
-//
-//
-//	return 0;
-//}
-
-////6.숫자만 추출
-//#include<stdio.h>
-//
-//int main() {
-//	//freopen("input.txt", "rt", stdin);
-//	char ch[60];
-//	int num = 0;
-//	int ten = 0;
-//	int cnt = 0;
-//	scanf("%s", ch);
-//	for (int i = 0; ch[i]; ++i) {
-//		if (ch[i] > 47 && ch[i] < 58) {
-//			num = ((ch[i] - 48) + (num * 10));
-//			++ten;
-//		}
-//	}
-//
-//	int j = num;
-//	for (int i = 1; i < j; ++i) {
-//		if (num % i == 0) {
-//			j = num / i;
-//			cnt += 2;;
-//		}
-//	}
-//	printf("%d\n", num);
-//	printf("%d", cnt);
-//
-//
-//	return 0;
-//}
-
-////5.나이계산
-//#include<stdio.h>
-//int main() {
-//	//freopen("input.txt", "rt", stdin);
-//	//freopen("output.txt", "wt", stdout);
-//	char ch[15] ;
-//	int year;
-//	int age;
-//	scanf("%s", ch);
-//	if (ch[0] != '0')
-//	{
-//		year = (ch[0]-48) * 10 + (ch[1]-48) + 1900;
-//		age = 2019 - year + 1;
-//		printf("%d ", age);
-//		if (ch[7] == '1')
-//			printf("M");
-//		else
-//			printf("W");
-//	}
-//	else {
-//		year = (ch[0]-48) * 10 + (ch[1]-48) + 2000;
-//		age = 2019 - year + 1;
-//		printf("%d ", age);
-//		if (ch[7] == '3')
-//			printf("M");
-//		else
-//			printf("W");
-//	}
-//
-//	return 0;
-//}
-
-////4.나이차
-//#include<iostream>
-//using namespace std;
-//int main() {
-//	//freopen("input.txt", "rt", stdin);
-//	//freopen("output.txt", "wt", stdout);
-//	int n, i, a, max = -2147000000, min = 2147000000;
-//	scanf_s("%d", &n);
-//
-//	for (i = 1; i <= n; i++) {
-//		cin >> a;
-//		if (a > max) max = a;
-//		if (a < min) min = a;
-//	}
-//	cout << max - min;
-//	return 0;
-//}
-
-////4. 나이차
-//#include <stdio.h>
-//#include <stdlib.h>
-//int main()
-//{
-//	int max, min;
-//	int T;
-//	scanf("%d", &T);
-//
-//	int* pt = (int*)malloc(sizeof(int) * T);
-//
-//	for (int i = 0; i < T; ++i) {
-//		scanf("%d",pt+i);
-//	}
-//	max = pt[0];
-//	min = pt[0];
-//
-//	for (int j = 0; j < T; ++j) {
-//		if (max < pt[j])
-//			max = pt[j];
-//		if (min > pt[j])
-//			min = pt[j];
-//	}
-//
-//	printf("%d", max - min);
-//
-//	free(pt);
-//
-//}
-
-
-////3. 진약수
-//#include <stdio.h>
-//#define MAX_N 100
-//
-//int num[MAX_N] = { 0 };
-//int main()
-//{
-//
-//	int i, j,
-//	int n;
-//	scanf_s("%d", &n);
-//	j = 0;
-//	int sum = 1;
-//	printf("1");
-//	for (i = 2; i < n; ++i) {
-//		if (n % i == 0) {
-//			sum += i;
-//			printf("+%d",i);
-//		}
-//	}
-//	
-//	printf("=%d", sum);
-//}
-
-////3. 진약수
-//#include <stdio.h>
-//#define MAX_N 100
-//
-//int num[MAX_N] = { 0 };
-//int main()
-//{
-//
-//	int i, j, mod;
-//	int sum = 0;
-//	int n;
-//	scanf_s("%d", &n);
-//	j = 0;
-//	for (i = 1; i < n; ++i) {
-//		if (n % i == 0){
-//			sum += i;
-//			num[j] = i;
-//			++j;
-//		}
-//	}
-//	--j;
-//	for (int k = 0; k <= j; ++k)
-//		if (k == j)
-//			printf("%d=", num[k]);
-//		else
-//			printf("%d+", num[k]);
-//
-//	printf("%d", sum);
-//}
-
-//#include <stdio.h>
-//#define MAX_N 100
-//
-//int num[MAX_N] = { 0 };
-//int main()
-//{
-//
-//	int i, j, mod;
-//	int sum = 0;
-//	int n;
-//	scanf_s("%d", &n);
-//	j = n;
-//	for (i = 1; i < j; ++i) {
-//		mod = n % i;
-//		if (mod == 0) {
-//			j = n/i;
-//			sum += i;
-//			sum += j;
-//		}
-//	}
-//
-//	printf("=%d", sum-n);
-//}
-
-//#include <stdio.h>
-//#define MAX_N 100
-//
-//int main()
-//{
-//	int a, b, sum = 0;
-//	scanf("%d %d", &a, &b);
-//
-//	for (int i = a; i <= b; ++i) {
-//		sum += i;
-//		if (i != b)
-//			printf("%d+", i);
-//		else
-//			printf("%d=");
-//	}
-//	printf("%d",sum);
-//}
-
-//// AA.cpp : This file contains the 'main' function. Program execution begins and ends there.
-////
-//
-//#include <iostream>
-//
-//int main()
-//{
-//	int a, b;
-//	int sum = 0;
-//
-//	scanf_s("%d%d",&a,&b);
-//
-//
-//	for (int i = 1; i <= a; ++i) {
-//		if(i%b==0)
-//			sum += i;
-//	}
-//
-//	printf("%d\n",sum);
-//
-//}
-//
