479e0ed060baa3e5e0f11e337ab86ec1e9361cfa
asus <kiwon1011@naver.com>
2021-01-06 10:15:48 +0900

    1/6 recursioin

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,24 +1,46 @@
-//7.영어단어복구-강의풀이
+//56. 재귀함수 분석
+
 #include <stdio.h>
-int main() {
-	//freopen("input.txt", "rt", stdin);
-	char str[101], restr[101];
 
-	gets_s(str);
-	int j = 0;
-	for (int i = 0; str[i]; ++i) {
-		if (str[i] != ' ') {
-			if (str[i] >= 65 && str[i] <= 90)//A: 65, Z: 90
-				restr[j++] = str[i] + 32; //후위 증감 대입하고 j는 1 증가
-			else
-				restr[j++] = str[i]; 
-		}
-	}
-	restr[j] = '\0'; //문자열 마지막에 널 문자 입력으로 %s로 출력하기 위해
-	printf("%s",restr);
+void RecursionNumber(int n) {
+	if (n == 0)
+		return;
 
-	return 0;
+	RecursionNumber(n - 1);
+
+	printf("%d ", n);
 }
+
+
+int main() {
+	freopen("input.txt", "rt", stdin);
+	int number;
+	scanf("%d", &number);
+	RecursionNumber(number);
+}
+
+
+////7.영어단어복구-강의풀이
+//#include <stdio.h>
+//int main() {
+//	//freopen("input.txt", "rt", stdin);
+//	char str[101], restr[101];
+//
+//	gets_s(str);
+//	int j = 0;
+//	for (int i = 0; str[i]; ++i) {
+//		if (str[i] != ' ') {
+//			if (str[i] >= 65 && str[i] <= 90)//A: 65, Z: 90
+//				restr[j++] = str[i] + 32; //후위 증감 대입하고 j는 1 증가
+//			else
+//				restr[j++] = str[i]; 
+//		}
+//	}
+//	restr[j] = '\0'; //문자열 마지막에 널 문자 입력으로 %s로 출력하기 위해
+//	printf("%s",restr);
+//
+//	return 0;
+//}
 
 ////7.영어단어복구
 //#include <stdio.h>
