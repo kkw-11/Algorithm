0e6dba14c25eabdb6a82d8ca1261edfe9a89ede3
asus <kiwon1011@naver.com>
2021-01-08 17:15:04 +0900

    1/8

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -19,33 +19,33 @@
 //	RecursionNumber(number);
 //}
 
-//8.올바른 괄호 -강의
-#include <stdio.h>
-int main() {
-	char ch[100];
-	int laststring = 0;
-	int cnt = 0;
-	scanf("%s", ch);
-
-	for (int i = 0; ch[i]; ++i) {
-		if (ch[i] == '(')
-			++cnt;
-		else if (ch[i] == ')')
-			--cnt;
-		else
-			cnt = -1;
-
-		if (cnt < 0)
-			break;
-	}
-
-	if (cnt == 0)
-		printf("YES\n");
-	else
-		printf("NO\n");
-
-	return 0;
-}
+////8.올바른 괄호 -강의
+//#include <stdio.h>
+//int main() {
+//	char ch[100];
+//	int laststring = 0;
+//	int cnt = 0;
+//	scanf("%s", ch);
+//
+//	for (int i = 0; ch[i]; ++i) {
+//		if (ch[i] == '(')
+//			++cnt;
+//		else if (ch[i] == ')')
+//			--cnt;
+//		else
+//			cnt = -1;
+//
+//		if (cnt < 0)
+//			break;
+//	}
+//
+//	if (cnt == 0)
+//		printf("YES\n");
+//	else
+//		printf("NO\n");
+//
+//	return 0;
+//}
 
 ////8.올바른 괄호
 //#include <stdio.h>
