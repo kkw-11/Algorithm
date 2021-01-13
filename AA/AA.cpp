0f9667ae7ebd3288663e992fb2a795a3f0ef5b73
asus <kiwon1011@naver.com>
2021-01-13 11:51:26 +0900

    1/13 Q9

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -5,17 +5,16 @@ int main() {
 	int n;
 	scanf("%d", &n);
 	for (int i = 1; i <= n; ++i) {
-		int num = i;
 		int cnt = 0;
-		int quotient = num;
+		int quotient = i;
 
 		//약수 개수 구하기
-		if (i == 1)//1의 약수 개수 무조건 1
+		if (i == 1)//1의 약수 개수 무조건 1 // else 조건으로 반복문 실행하면 0 이 출력됨
 			printf("%d ", 1);
 		else {//2부터 약수 개수 구하기 반복문 횟수 줄이기 위해서 나누는 값으 1부터 n 까지 하지말고 몫 직전까지 
 			for (int j = 1; j < quotient; ++j) {
-				if (num % j == 0) {
-					quotient = num / j;
+				if (i % j == 0) {
+					quotient = i / j;
 					if (j == quotient)  
 						cnt += 1; // 제곱수의 경우 약수 개수 1개 증가
 					else
