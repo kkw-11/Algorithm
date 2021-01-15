691d07b6b9e76ecc1d432be4486e9c79b7f54511
asus <kiwon1011@naver.com>
2021-01-15 15:12:56 +0900

    1/15 Q11

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -3,44 +3,70 @@
 
 int main() {
 	//freopen("input.txt", "rt", stdin);
-	int num, cnt = -1, res = 0;
+	int num;
 	scanf("%d", &num);
-	int digit = num;
+	int digit;
+	int cnt = 0;
+	for (int i = 1; i <= num; ++i) {		
+		digit = i;
 
-	//자리수 구하기
-	while (digit > 0) {
-		digit = digit / 10;
-		++cnt;
+		//각 숫자가 몇자리인지 카운팅 누적
+		while (digit > 0) {
+			digit = digit / 10;
+			++cnt;
+		}
 	}
 
-	switch (cnt)
-	{
-	case 0:
-		res = 1 * num;
-		break;
-	case 1:
-		res = 1 * 9 + 2 * (num - 9);
-		break;
-	case 2:
-		res = 1 * 9 + 2 * 90 + 3 * (num - 99);
-		break;
-	case 3:
-		res = 1 * 9 + 2 * 90 + 3 * 900 + 4 * (num - 999);
-		break;
-	case 4:
-		res = 1 * 9 + 2 * 90 + 3 * 900 + 4 * 9000 + 5 * (num - 9999);
-		break;
-	case 5:
-		res = 1 * 9 + 2 * 90 + 3 * 900 + 4 * 9000 + 5 * 90000 + 6 * (num - 99999);
-		break;
-	default:
-		break;
-	}
 
-	printf("%d", res);
+	printf("%d", cnt);
 
 	return 0;
 }
+
+
+////11.자릿수의 합
+//#include <stdio.h>
+//
+//int main() {
+//	//freopen("input.txt", "rt", stdin);
+//	int num, cnt = -1, res = 0;
+//	scanf("%d", &num);
+//	int digit = num;
+//
+//	//자리수 구하기
+//	while (digit > 0) {
+//		digit = digit / 10;
+//		++cnt;
+//	}
+//
+//	switch (cnt)
+//	{
+//	case 0:
+//		res = 1 * num;
+//		break;
+//	case 1:
+//		res = 1 * 9 + 2 * (num - 9);
+//		break;
+//	case 2:
+//		res = 1 * 9 + 2 * 90 + 3 * (num - 99);
+//		break;
+//	case 3:
+//		res = 1 * 9 + 2 * 90 + 3 * 900 + 4 * (num - 999);
+//		break;
+//	case 4:
+//		res = 1 * 9 + 2 * 90 + 3 * 900 + 4 * 9000 + 5 * (num - 9999);
+//		break;
+//	case 5:
+//		res = 1 * 9 + 2 * 90 + 3 * 900 + 4 * 9000 + 5 * 90000 + 6 * (num - 99999);
+//		break;
+//	default:
+//		break;
+//	}
+//
+//	printf("%d", res);
+//
+//	return 0;
+//}
 
 
 ////10.자릿수의 합
