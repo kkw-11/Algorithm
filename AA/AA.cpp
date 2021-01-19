2978d56cfde5f2d1dec6b2d0bc77ba9931547eb4
asus <kkw-11@naver.com>
2021-01-19 23:04:28 +0900

    1/19 Q34

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,43 +1,73 @@
-//32. 3등 성적은?
+//34 버블정렬
+void Swap(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
 #include <stdio.h>
 int main() {
 	int T;
-	int grade[100] = { 0 };
-	int minIdx;
 	scanf("%d", &T);
+	int num[100];
 	for (int i = 0; i < T; ++i) {
-		scanf("%d", &grade[i]);
+		scanf("%d", &num[i]);
 	}
-	//오름차순 정렬
-	for (int i = 0; i < T - 1; ++i) {
-		minIdx = i;
-		for (int j = i + 1; j < T; ++j) {
-			if (grade[minIdx] > grade[j]) {
-				int temp = grade[minIdx];
-				grade[minIdx] = grade[j];
-				grade[j] = temp;
+	for (int i = T - 1; i > 0; --i) {
+		for (int j = 0; j < i; ++j) {
+			if (num[j] > num[j + 1]) {
+				Swap(&num[j], &num[j + 1]);
 			}
+
 		}
 	}
+		
 
-	//중복 값 존재 할때 3등 구하기
-	int max = grade[T - 1];
-	int cnt = 1;
-	for (int i = T - 1; i >= 0; --i) {
-		if (grade[i] < max) {
-			++cnt;
-			max = grade[i];
-		}
-		if (cnt == 3)
-			break;
-	}
-
-	printf("%d", max);
 
 
 	return 0;
 }
+
+////32. 3등 성적은?
+//
+//#include <stdio.h>
+//int main() {
+//	int T;
+//	int grade[100] = { 0 };
+//	int minIdx;
+//	scanf("%d", &T);
+//	for (int i = 0; i < T; ++i) {
+//		scanf("%d", &grade[i]);
+//	}
+//	//오름차순 정렬
+//	for (int i = 0; i < T - 1; ++i) {
+//		minIdx = i;
+//		for (int j = i + 1; j < T; ++j) {
+//			if (grade[minIdx] > grade[j]) {
+//				int temp = grade[minIdx];
+//				grade[minIdx] = grade[j];
+//				grade[j] = temp;
+//			}
+//		}
+//	}
+//
+//	//중복 값 존재 할때 3등 구하기
+//	int max = grade[T - 1];
+//	int cnt = 1;
+//	for (int i = T - 1; i >= 0; --i) {
+//		if (grade[i] < max) {
+//			++cnt;
+//			max = grade[i];
+//		}
+//		if (cnt == 3)
+//			break;
+//	}
+//
+//	printf("%d", max);
+//
+//
+//	return 0;
+//}
 
 ////32 선택정렬
 //#include <stdio.h>
