c4574f19e688d3b09f5a18565d57329deebbc526
asus <kkw-11@naver.com>
2021-01-26 12:38:32 +0900

    1/26

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,34 +1,118 @@
-//13. 가장 많이 사용된 자릿수
+//14. 뒤집은 소수
 #include <stdio.h>
+bool isPrime(int x) {
+	if (x == 1) return 0;
+	int prime = 1;
+
+	for (int i = 2; i < x; ++i) {
+		if ((x % i) == 0) {
+			prime = 0;
+			return prime;
+		}
+
+	}
+
+	return prime;
+}
+
+
+int reverse(int x) {
+	int i = 1,k, digit, res = 0,ten=1;
+	//10배수 0 없애기
+	while (1) {
+		if (x % 10 == 0) {
+			x /= 10;
+		}
+		else break;
+	}
+	digit = int (x / 10);
+
+	//자리수 구하기
+	while (digit) {
+		digit = int(digit / 10);
+		++i;
+	}
+
+	//제일 끝자리가 몇의 자리인지 표시
+	for (int j = 1; j < i; ++j) {
+		ten *= 10;
+	}
+
+	//뒤집기
+	int temp = x, mod;
+	for (int j = 0; j < i; ++j) {
+		mod = temp % 10;
+		temp = (int)temp / 10;
+
+		mod *= ten;
+		ten /= 10;
+		res += mod;
+
+	}
+
+		
+	/*	
+	while () {
+		mod = x % 10;
+
+	}*/
+
+
+	return res;
+}
 int main() {
 	//freopen("input.txt", "rt", stdin);
-	char num[100] = { 0 };
-	scanf("%s", num);
-	int digit[10] = { 0 };
-	int mod = 0, max = -1, maxIndex;
+	int n,value;
+	int num[100];
+	scanf("%d", &n);
 	
-	for (int i = 0; num[i]; ++i) {
-		int check = num[i] - 48;
-		++digit[check];
-	}
-
-	for (int i = 9; i >= 0; --i) {
-		if (max <= digit[i]) {
-			max = digit[i];
-			maxIndex = i;
+	for (int i = 0; i < n; ++i) {
+		scanf("%d", &value);
+	
+		//뒤집는다.
+		value = reverse(value);
+		
+		//뒤집은 값 소수 판정
+		if (isPrime(value)) {
+			printf("%d ", value);
 		}
+		
 	}
 
-	//for (int i = 9; i >= 0; --i) {
-	//	if (max == digit[i]) {
-	//		maxIndex = i;
-	//		break;
-	//	}
-	//}
-
-	printf("%d", maxIndex);
 	return 0;
 }
+
+////13. 가장 많이 사용된 자릿수
+//#include <stdio.h>
+//int main() {
+//	//freopen("input.txt", "rt", stdin);
+//	char num[100] = { 0 };
+//	scanf("%s", num);
+//	int digit[10] = { 0 };
+//	int mod = 0, max = -1, maxIndex;
+//	
+//	for (int i = 0; num[i]; ++i) {
+//		int check = num[i] - 48;
+//		++digit[check];
+//	}
+//
+//	for (int i = 9; i >= 0; --i) {
+//		if (max <= digit[i]) {
+//			max = digit[i];
+//			maxIndex = i;
+//		}
+//	}
+//
+//	//for (int i = 9; i >= 0; --i) {
+//	//	if (max == digit[i]) {
+//	//		maxIndex = i;
+//	//		break;
+//	//	}
+//	//}
+//
+//	printf("%d", maxIndex);
+//	return 0;
+//}
 
 ////13. 가장 많이 사용된 자릿수
 //#include <stdio.h>
