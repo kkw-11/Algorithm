25131b1a5d71e6b6ce3189604172627b634356d4
asus <kkw-11@naver.com>
2021-02-04 11:21:59 +0900

    2/4 Q23

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,3 +1,63 @@
+//23. 연속 부분 증가수열
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	int n, cnt = 1, max = cnt;
+	scanf("%d", &n);
+	int* p = (int*)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; ++i) {
+		scanf("%d", &p[i]);
+	}
+
+	for (int i = 0; i < n-1; ++i) {
+		if (p[i] <= p[i + 1]) {
+			++cnt;
+			if (max <= cnt)
+				max = cnt;
+		}
+		else {
+			cnt = 1;
+		}
+	}
+
+	printf("%d", max);
+	return 0;
+}
+
+////22. 온도의 최대값
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main() {
+////	freopen("input.txt", "rt", stdin);
+//
+//	int N, K;
+//	int sum = 0, max = -21400000;
+//	scanf("%d %d", &N, &K);
+//
+//	int* p =(int*)malloc(sizeof(int) * N);
+//	for (int i = 0; i < N; ++i) {
+//		scanf("%d", &p[i]);
+//		//온도값 입력 받으면서 첫번째 구간의 합 먼저 구하기
+//		if (i < K)
+//			sum += p[i];
+//	}
+//
+//	max = sum;
+//	//i는 더해지는 값을 기준으로 생각해서 초기값 설정
+//	for (int i = K; i < N; ++i) {
+//		//구간에서 하나 추가된 값과 하나전값 빼서 구간합 구하기
+//		sum += (p[i] - p[i-K]);
+//		if (sum > max)
+//			max = sum;
+//	}
+//	printf("%d", max);
+//	free(p);
+//
+//	return 0;
+//}
+//
+//
 ////22. 온도의 최대값
 //#include <stdio.h>
 //int main() {
@@ -7,6 +67,8 @@
 //	int temper[100000];
 //	int sum = 0, max = -21400000;
 //	scanf("%d %d", &N, &K);
+//
+//	//i는 빠지는 값을 기준으로 생각해서 초기값 설정
 //
 //	for (int i = 0; i < N; ++i) {
 //		scanf("%d", &temper[i]);
