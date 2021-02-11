92ad29adf3f4903966c8df430c15367416d8e0f4
asus <kkw-11@naver.com>
2021-02-11 10:49:26 +0900

    2/11 Q26

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,3 +1,63 @@
+//26. 마라톤
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+	int n;
+	scanf("%d", &n);
+	int* p = (int*)malloc(sizeof(int) * n);
+	int cnt = 0;
+	for (int i = 0; i < n; ++i) {
+		scanf("%d", &p[i]);
+	}
+
+	for (int i = 0; i < n; ++i) {
+		cnt = 1;
+		for (int j = 0; j < i; ++j) {
+			if (p[i] <= p[j])
+				++cnt;
+		}
+		printf("%d ", cnt);
+	}
+
+	free(p);
+}
+
+
+////25. 석차 구하기
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main() {
+//	//freopen("input.txt", "rt", stdin);
+//	int n, cnt = 0;
+//	scanf("%d", &n);
+//	int* p1 = (int*)malloc(sizeof(int) * n);
+//	int* p2 = (int*)malloc(sizeof(int) * n);
+//
+//	//점수배열과, 등수표시 배열값 입력
+//	//등수표시 배열은 초기값 1로 설정
+//	for (int i = 0; i < n; ++i) {
+//		scanf("%d", &p1[i]);
+//		p2[i] = 1;
+//	}
+//
+//
+//	for (int i = 0; i < n; ++i) {
+//		for (int j = 0; j < n; ++j) {
+//			if (p1[i] < p1[j])
+//				++p2[i];
+//		}
+//	}
+//
+//	for (int i = 0; i < n; ++i) {
+//		printf("%d ",p2[i]);
+//	}
+//	free(p1);
+//	free(p2);
+//
+//	return 0;
+//}
+//
 ////25. 석차 구하기
 //#include <stdio.h>
 //#include <stdlib.h>
@@ -13,7 +73,6 @@
 //	printf("\n");
 //}
 //void _Sort(int list[], int left, int right) {
-//	Print(list, 5);
 //
 //	if (left <= right) {
 //		int pivot = left;
@@ -21,19 +80,22 @@
 //		int j = right;
 //
 //		do {
-//			if (list[pivot] >= list[i])
+//			while (list[pivot] > list[i])
 //				++i;
-//			if (list[pivot] <= list[j])
+//			while (list[pivot] < list[j])
 //				--j;
 //
 //			if (i <= j) {
 //				Swap(&list[i], &list[j]);
+//				++i;
+//				--j;
 //			}
-//
+//			Print(list, 5);
 //		} while (i <= j);
-//		Print(list, 5);
 //
 //		Swap(&list[pivot], &list[j]);
+//		Print(list, 5);
+//
 //		_Sort(list, left, j - 1);
 //		_Sort(list, j + 1, right);
 //
@@ -47,7 +109,7 @@
 //
 //int main() {
 //	freopen("input.txt", "rt", stdin);
-//	int n;
+//	int n, cnt = 0;
 //	scanf("%d", &n);
 //	int* p1 = (int*)malloc(sizeof(int) * n);
 //	int* p2 = (int*)malloc(sizeof(int) * n);
@@ -56,17 +118,29 @@
 //		scanf("%d", &p1[i]);
 //		p2[i] = p1[i];
 //	}
-//	Print(p2, n);
+//	Print(p1, 5);
+//	
 //	//정렬
 //	Sort(p2, n);
-//	Print(p2, n);
 //
 //
-//	//p1 첫인덱스부터 정렬된 p2의 값을 찾아서 등수 출력
+//	//p1 첫 인덱스부터 정렬된 p2의 값을 찾아서 등수 출력
+//	for (int i = 0; i < n; ++i) {
+//		cnt = 1;
+//		for (int j = n - 1; j >= 0; --j) {
+//			if (p1[i] == p2[j]) {
+//				printf("%d ", cnt);
+//				break;
+//			}
+//			++cnt;
+//		}
+//	}
+//	free(p1);
+//	free(p2);
 //
 //	return 0;
 //}
-
+//
 
 ////24. Jolly Jumpers
 //#include <stdio.h>
