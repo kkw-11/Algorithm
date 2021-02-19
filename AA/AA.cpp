e0a718a5b0a19b72e0b1c1502bc79fbe8523ef64
asus <kkw-11@naver.com>
2021-02-19 15:48:05 +0900

    2/19 Q30

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -84,31 +84,44 @@
 #include <stdio.h>
 int main() {
 	freopen("input.txt", "rt", stdin);
-	int n, sum = 0, digit = 0, i, temp;
+	int n, cntThree = 0, cntTotal = 0, digit = 0, i, temp = 1, digitStartnum;
 	scanf("%d", &n);
-	if (n >= 3) {
-		for (i = 1; i <= n; i *= 10) {
-			sum = sum * 9 + i;
+	temp = n;
+	//자리수 구하기
+	while (temp > 0) {
+		++digit;
+		temp /= 10;
+	}
+
+	digitStartnum = 1;
+	if (n >= 10) {
+		//구한 자리수 아래까지는 규칙대로 3 구하기
+		for (i = 1; i < digit; ++i) {
+			cntThree = cntThree * 10 + digitStartnum;
+			cntTotal += cntThree;
+			digitStartnum *= 10;
 		}
 
-		digit = i / 10;
-		i /= 10;
-
-		for (i = digit + 1; i <= n; ++i) {
+		for (i = digitStartnum; i <= n; ++i) {
 			temp = i;
 			while (temp > 0) {
 				if (temp % 10 == 3) {
-					++sum;
+					++cntTotal;
 				}
 				temp /= 10;
 
 			}
 		}
 	}
-	printf("%d", sum);
+	else if (n < 10 && n >= 3) {
+		cntTotal = 1;
+	}
+
+	printf("%d", cntTotal);
 
 	return 0;
 }
+
 
 ////29.3의 개수는?(small)
 //#include <stdio.h>
@@ -123,7 +136,11 @@ int main() {
 //			if (temp % 10 == 3) {
 //				++cnt;
 //			}
-//			temp /= 10;
+//			
+//				temp /= 10;
+//		}
+//		if (i % 10 == 0) {
+//			printf("number:%d->%d\n", i, cnt);
 //		}
 //
 //	}
@@ -131,7 +148,7 @@ int main() {
 //	return 0;
 //
 //}
-//
+
 
 
 ////29.3의 개수는?(small)
