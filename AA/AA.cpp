831e52bcedb11c18bb6eac54d54f8718725d4948
asus <kkw-11@naver.com>
2021-04-04 02:02:57 +0900

    Q 47

--- AA/AA.cpp
+++ AA/AA.cpp
@@ -1,7 +1,7 @@
 //47.봉우리(2차원 배열 탐색)
 #include<stdio.h>
 //전역 변수로 선언하면 자동으로 초기값 0 
-int map[51][51];
+int map[52][52];
 int dx[] = { 0,0,-1,1 };
 int dy[] = { -1,1,0,0 };
 int main() {
@@ -38,33 +38,33 @@ int main() {
 }
 
 ////47.봉우리(2차원 배열 탐색)
-//#include<stdio.h>
-////전역 변수로 선언하면 자동으로 초기값 0 
-//int map[51][51];
-//int main() {
-//	//freopen("input.txt", "rt", stdin);
-//
-//	int n;
-//	int cnt = 0;
-//	scanf("%d", &n);
-//	for (int i = 1; i <= n; ++i) {
-//		for (int j = 1; j <= n; ++j) {
-//			scanf("%d", &map[i][j]);
-//		}
-//	}
-//
-//	for (int i = 1; i <= n; ++i) {
-//		for (int j = 1; j <= n; ++j) {
-//			if ((map[i][j] > map[i - 1][j]) && (map[i][j] > map[i + 1][j]) && (map[i][j] > map[i][j - 1]) && (map[i][j] > map[i][j + 1]))
-//				++cnt;
-//		}
-//	}
-//
-//	printf("%d", cnt);
-//
-//
-//	return 0;
-//}
+#include<stdio.h>
+//전역 변수로 선언하면 자동으로 초기값 0 
+int map[52][52];
+int main() {
+	//freopen("input.txt", "rt", stdin);
+
+	int n;
+	int cnt = 0;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			scanf("%d", &map[i][j]);
+		}
+	}
+
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			if ((map[i][j] > map[i - 1][j]) && (map[i][j] > map[i + 1][j]) && (map[i][j] > map[i][j - 1]) && (map[i][j] > map[i][j + 1]))
+				++cnt;
+		}
+	}
+
+	printf("%d", cnt);
+
+
+	return 0;
+}
 
 ////41.연속된 자연수의 합
 //#include<stdio.h>
